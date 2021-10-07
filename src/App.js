55994fbbaf19783dc79a94a0048fe2b4949f9ab7
richardingwe <richardingwe5@gmail.com>
2021-10-07 08:20:09 +0100

    added routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from 'react-router';
+import { BooksList } from './BooksList';
+import { CreateBook } from './CreateBook';
+import { UpdateBook } from './UpdateBook';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route path='/update-book'>
+        <UpdateBook />
+      </Route>
+      <Route path='/create-book'>
+        <CreateBook />
+      </Route>
+      <Route path='/'>
+        <BooksList />
+      </Route>
+    </Switch>
   );
 }
 
