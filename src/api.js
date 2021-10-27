fbb16b0859afe3fdcacd6a2a2e0cacd88aa74966
richardingwe <richardingwe5@gmail.com>
2021-10-27 15:07:04 +0100

    added the updatebook function

--- src/api.js
+++ src/api.js
@@ -20,6 +20,25 @@ export const getBook = async ({ queryKey }) => {
     return response.json();
 };
 
+export const updateBook = async ({ id, ...data }) => {
+    const response = await fetch(
+        `${process.env.REACT_APP_API_SERVER}/books/${id}`,
+        {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        }
+    );
+
+    if (!response.ok) {
+        throw new Error(response.json().message);
+    }
+
+    return response.json();
+};
+
 export const removeBook = async (id) => {
     const response = await fetch(
         `${process.env.REACT_APP_API_SERVER}/books/${id}`,
