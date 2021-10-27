6cb797ac6b8dc87dceb7aa0c0e9e2ce6916de9f1
richardingwe <richardingwe5@gmail.com>
2021-10-27 14:45:51 +0100

    added a removeBook API function

--- src/api.js
+++ src/api.js
@@ -8,3 +8,17 @@ export const getAllBooks = async () => {
     return response.json();
 };
 
+export const removeBook = async (id) => {
+    const response = await fetch(
+        `${process.env.REACT_APP_API_SERVER}/books/${id}`,
+        {
+            method: "DELETE"
+        }
+    );
+
+    if (!response.ok) {
+        throw new Error(response.json().message);
+    }
+
+    return true;
+};
